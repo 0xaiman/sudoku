5fb882abae8ea5dcbc0809fbf58cac96bea98b8c
0xaiman <aimandani9797@gmail.com>
2024-03-04 12:53:40 +0800

    02- add sudoku input field

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function shuffleArray(array){
 }
 
 
-function checkValid(array){
+function checkValidRow(array){
     ///scans each row
 for(let i=0; i<9; i+=3){
     let arrayHolderRow =[]
@@ -39,73 +39,40 @@ for(let i=0; i<9; i+=3){
     let uniqueEl = new Set(arrayHolderRow)
 if (uniqueEl.size === arrayHolderRow.length){
     console.log('Row Element OK');
-    return array
+    checkValidCol(array)
 
 }else{
     console.log('Row elements are not unique');
-    return checkValid(shuffleArray(array));
+    return checkValidRow(shuffleArray(array));
 }
 
 }
 
+}
 
 
-// for(let i=0; i<3; i++){
-//     for(let j=0; j<3; j++){
-//     arrayHolderRow.push(array[i][j])
+function checkValidCol(array){
+    ///scans each row
+for(let i=0; i<9; i+=3){
+    let arrayHolderRow =[]
+    for(let j=0; j<3; j++){
+        for(let k=0; k<3; k++){
+            arrayHolderRow.push(array[k][i+j]);
+        }
 
-//     }
-// }
-
-
-// let uniqueEl = new Set(arrayHolderRow)
-// if (uniqueEl.size === arrayHolderRow.length){
-//     console.log('Row Element OK');
-
+    }
+    let uniqueEl = new Set(arrayHolderRow)
     
+if (uniqueEl.size === arrayHolderRow.length){
+    console.log('Col Element OK');
+    return array
 
-// }else{
-//     console.log('Row elements are not unique');
-//     return checkValid(shuffleArray(array));
-// }
+}else{
+    console.log('Col elements are not unique');
+    return checkValidCol(shuffleArray(array));
+}
 
-// arrayHolderRow=[]
-// for(let i=3; i<6; i++){
-//     for(let j=0; j<3; j++){
-//     arrayHolderRow.push(array[i][j])
-
-//     }
-// }
-
-// uniqueEl = new Set(arrayHolderRow)
-// if (uniqueEl.size === arrayHolderRow.length){
-//     console.log('Row Element OK');
-
-
-// }else{
-//     console.log('Row elements are not unique');
-//     return checkValid(shuffleArray(array));
-// }
-// arrayHolderRow=[]
-// for(let i=6; i<9; i++){
-//     for(let j=0; j<3; j++){
-//     arrayHolderRow.push(array[i][j])
-
-//     }
-// }
-
-// uniqueEl = new Set(arrayHolderRow)
-// if (uniqueEl.size === arrayHolderRow.length){
-//     console.log('Row Element OK');
-
-//     return array
-
-// }else{
-//     console.log('Row elements are not unique');
-//     return checkValid(shuffleArray(array));
-// }
-
-
+}
 
 }
 
@@ -119,12 +86,15 @@ const boxIds = ["box-1", "box-2", "box-3", "box-4", "box-5", "box-6", "box-7", "
 const boxElements = boxIds.map(id => document.getElementById(id));
 
 
-document.addEventListener("DOMContentLoaded", e=>{
+document.addEventListener("DOMContentLoaded", function(){
     // console.log(numbers)
     generateSudokuBoard();
     shuffleArray(board)
-    checkValid(board)
-    sudokuStart(board)
+    checkValidRow(board)
+    // checkValidCol(board)
+    sudokuStart(board);
+    keyCheck();
+
 
 
     // console.log(board)
@@ -137,24 +107,60 @@ document.addEventListener("DOMContentLoaded", e=>{
 function sudokuStart(board){
     for(let i = 0; i<boxElements.length ; i++){
         boxElements[i].innerHTML = `
-        <tr> 
-            <td id="cell-01">${board[i][0]}</td>
-            <td id="cell-02">${board[i][1]}</td>
-            <td id="cell-03">${board[i][2]}</td>
+        <tr>
+            <td><input   type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-0"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-1"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-2"/></td>
         </tr>
-        <tr> 
-            <td id="cell-04">${board[i][3]}</td>
-            <td id="cell-05">${board[i][4]}</td>
-            <td id="cell-06">${board[i][5]}</td>
+        <tr>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-3"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-4"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-5"/></td>
         </tr>
-        <tr> 
-            <td id="cell-07">${board[i][6]}</td>
-            <td id="cell-08">${board[i][7]}</td>
-            <td id="cell-09">${board[i][8]}</td>
+        <tr>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-6"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-7"/></td>
+            <td><input  type="text" maxlength="1"  onkeypress="return /[0-9]/i.test(event.key)" id="cell-${i}-8"/></td>
         </tr>
         `
-    }
+
+
+    //     boxElements[i].innerHTML = `
+    //     <tr> 
+    //         <td id="cell-${i}-1"></td>
+    //         <td id="cell-${i}-2"></td>
+    //         <td id="cell-${i}-3"></td>
+    //     </tr>
+    //     <tr> 
+    //         <td id="cell-${i}-4"></td>
+    //         <td id="cell-${i}-5"></td>
+    //         <td id="cell-${i}-6"></td>
+    //     </tr>
+    //     <tr> 
+    //         <td id="cell-${i}-7"></td>
+    //         <td id="cell-${i}-8"></td>
+    //         <td id="cell-${i}-9"></td>
+    //     </tr>
+    //     `
+    // 
+}
    
 
 }
 
+
+function keyCheck(){
+    var inputFields = document.querySelectorAll('input[type="text"]');
+    
+    inputFields.forEach(function(inputField) {
+        inputField.addEventListener("keypress", function(event) {
+            var keyCode = event.keyCode;
+            
+            // Allow numbers (0-9) and backspace (8) key
+            if ((keyCode < 48 || keyCode > 57) && keyCode !== 8) {
+                event.preventDefault();
+            }
+        });
+    });
+
+}
